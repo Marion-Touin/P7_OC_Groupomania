acc7b3144327e9901fdebc7b5bfc3c645a852d3c
Marion Touin <t.marion@hotmail.com>
2020-10-27 09:01:05 +0100

    connexion à la base de données avec sequelize

--- backend/connexiondb.js
+++ backend/connexiondb.js
@@ -1,18 +1,15 @@
-const mysql = require("mysql");
-const dbConfig = require("./configuration/db");
+const dotenv = require ('dotenv') . config ( )
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASS, { dialect: "mysql",  host: process.env.DB_HOST });
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+const dbTest = async function () {
+    try {
+      await sequelize.authenticate();
+      console.log('Connexion réussi !');
+    } catch (error) {
+      console.error('Connexion échouée !', error);
+    }
+  };
+  dbTest();
 
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Connexion à la base de données reussie !");
-});
-
-module.exports = connection;
+module.exports= sequelize
